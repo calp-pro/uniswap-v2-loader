f38e4051de8417805f95c0dcfa07d8d1895a6fe5
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-02-28 19:15:46 +0700

    test fn

--- default_cache_filename.js
+++ default_cache_filename.js
@@ -13,9 +13,9 @@ module.exports = (factory) => path.join(
       : process.platform === 'darwin'
         ? [home, 'Library', 'Caches']
         : env.XDG_CACHE_HOME && path.isAbsolute(env.XDG_CACHE_HOME) && fs.existsSync(env.XDG_CACHE_HOME)
-          ? [env.XDG_CACHE_HOME]
-          : fs.existsSync(path.join(home, '.cache'))
-            ? [home, '.cache']
-            : [os.tmpdir()],
+          ? console.log(1) || [env.XDG_CACHE_HOME]
+          : console.log(2) || fs.existsSync(path.join(home, '.cache'))
+            ? console.log(3) || [home, '.cache']
+            : console.log(4) || [os.tmpdir()],
   `${pkg.name}_${factory.toLowerCase()}.csv`
 )
