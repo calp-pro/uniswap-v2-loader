a1fe007df7a20933f3c486d322bb7d20b0bc5ba2
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-03-04 11:12:18 +0700

    test cache

--- default_cache_filename.js
+++ default_cache_filename.js
@@ -1,21 +1,20 @@
 const path = require('path')
-const {GITHUB_ACTIONS, XDG_CACHE_HOME, LOCALAPPDATA, APPDATA} = process.env
 const fs = require('fs')
 const os = require('os')
 const home = os.homedir()
 const pkg = require('./package.json')
 
 module.exports = factory => path.join(
-    ...(GITHUB_ACTIONS
+    ...(process.env.GITHUB_ACTIONS
         ? ['.']
         : process.platform == 'win32'
-            ? LOCALAPPDATA || APPDATA
-                ? [LOCALAPPDATA || APPDATA]
+            ? process.env.LOCALAPPDATA || process.env.APPDATA
+                ? [process.env.LOCALAPPDATA || process.env.APPDATA]
                 : [home, 'AppData', 'Local']
             : process.platform == 'darwin'
                 ? [home, 'Library', 'Caches']
-                : XDG_CACHE_HOME && path.isAbsolute(XDG_CACHE_HOME) && fs.existsSync(XDG_CACHE_HOME)
-                    ? [XDG_CACHE_HOME]
+                : process.env.XDG_CACHE_HOME && path.isAbsolute(process.env.XDG_CACHE_HOME) && fs.existsSync(process.env.XDG_CACHE_HOME)
+                    ? [process.env.XDG_CACHE_HOME]
                     : fs.existsSync(path.join(home, '.cache'))
                         ? [home, '.cache']
                         : [os.tmpdir()]
